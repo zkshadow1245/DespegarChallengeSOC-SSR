80b25ce0764cc658c8f0c1979cbd987d77d56a33
Ariel Alejandro Méndez <131214808+zkshadow1245@users.noreply.github.com>
2025-07-30 23:08:02 -0300

    Ejercicio 2

---
+++ ejercicio2_ABUSEIPDB.py
@@ -0,0 +1,9 @@
+"""
+Desarrollar un script que parsee el archivo alerta_waf.json, extraiga la IP de 
+origen y consulte la API del servicio AbuseIPDB para su análisis. A continuación, 
+redactar un informe detallado del evento, determinar si se trata de un incidente y, en 
+caso de serlo, proponer la remediación recomendada.
+  
+a. Incluir el código, la salida del script  
+b. Desarrollar un informe completo.
+"""
